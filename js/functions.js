ac7939417541fa8331eb15730678f717e7b0080e
Rishi Kothari <rishiosaur@gmail.com>
2017-10-30 10:51:57 -0400

    input

--- js/functions.js
+++ js/functions.js
@@ -1,6 +1,14 @@
 var html = {
-  input: function(question, output){
-    //stuff goes here
+  input: document.getElementById('input'),
+  input: function(event) {
+    var x = event.keyCode;
+    var p = document.getElementById('cmd');
+    var br = document.createElement('br');
+    var text = document.createTextNode(input.value);
+    if(x === 13){
+      p.appendChild(text);
+      p.appendChild(br);
+    }
   },
   button: function(name, action){
     //stuff goes here
